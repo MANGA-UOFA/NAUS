9e968e6c41270b8ecd0f13db81a006e584b80548
Puyuan Liu <puyuan@ualberta.ca>
2022-09-17 20:27:46 -0600

    Update preprocess_giga.sh

--- preprocess_giga.sh
+++ preprocess_giga.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python create_giga_HC_dictionary.py
 
-TEXT=gigaword_8
+TEXT=training_data/gigaword_8
 python fairseq_cli/preprocess.py --source-lang article --target-lang summary --trainpref $TEXT/train --validpref $TEXT/valid --testpref $TEXT/test,$TEXT/DUC2004 --destdir data-bin/$TEXT --workers 40 --joined-dictionary --task summarization --srcdict giga_HC.dict
 
 mv data-bin/$TEXT/test1.article-summary.article.bin data-bin/$TEXT/duc2004.article-summary.article.bin
@@ -9,7 +9,7 @@ mv data-bin/$TEXT/test1.article-summary.article.idx data-bin/$TEXT/duc2004.artic
 mv data-bin/$TEXT/test1.article-summary.summary.bin data-bin/$TEXT/duc2004.article-summary.summary.bin
 mv data-bin/$TEXT/test1.article-summary.summary.idx data-bin/$TEXT/duc2004.article-summary.summary.idx
 
-TEXT=gigaword_10
+TEXT=training_data/gigaword_10
 python fairseq_cli/preprocess.py --source-lang article --target-lang summary --trainpref $TEXT/train --validpref $TEXT/valid --testpref $TEXT/test,$TEXT/DUC2004 --destdir data-bin/$TEXT --workers 40 --joined-dictionary --task summarization --srcdict giga_HC.dict
 
 mv data-bin/$TEXT/test1.article-summary.article.bin data-bin/$TEXT/duc2004.article-summary.article.bin
@@ -17,7 +17,7 @@ mv data-bin/$TEXT/test1.article-summary.article.idx data-bin/$TEXT/duc2004.artic
 mv data-bin/$TEXT/test1.article-summary.summary.bin data-bin/$TEXT/duc2004.article-summary.summary.bin
 mv data-bin/$TEXT/test1.article-summary.summary.idx data-bin/$TEXT/duc2004.article-summary.summary.idx
 
-TEXT=gigaword_13
+TEXT=training_data/gigaword_13
 python fairseq_cli/preprocess.py --source-lang article --target-lang summary --trainpref $TEXT/train --validpref $TEXT/valid --testpref $TEXT/test,$TEXT/DUC2004 --destdir data-bin/$TEXT --workers 40 --joined-dictionary --task summarization --srcdict giga_HC.dict
 
 mv data-bin/$TEXT/test1.article-summary.article.bin data-bin/$TEXT/duc2004.article-summary.article.bin
@@ -26,7 +26,7 @@ mv data-bin/$TEXT/test1.article-summary.summary.bin data-bin/$TEXT/duc2004.artic
 mv data-bin/$TEXT/test1.article-summary.summary.idx data-bin/$TEXT/duc2004.article-summary.summary.idx
 
 python create_giga_ref_dictionary.py
-TEXT=gigaword_ref
+TEXT=training_data/gigaword_ref
 python fairseq_cli/preprocess.py --source-lang article --target-lang summary --trainpref $TEXT/train --validpref $TEXT/valid --testpref $TEXT/test,$TEXT/DUC2004 --destdir data-bin/$TEXT --workers 40 --joined-dictionary --task summarization --srcdict giga_ref.dict
 
 mv data-bin/$TEXT/test1.article-summary.article.bin data-bin/$TEXT/duc2004.article-summary.article.bin
