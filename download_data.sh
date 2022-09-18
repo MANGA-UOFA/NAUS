9a5428e28292ceee6d2350833314ffa752a16c0f
Puyuan Liu <puyuan@ualberta.ca>
2022-09-17 20:27:09 -0600

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 NAUS_data_link=https://drive.google.com/drive/folders/1XKN6oFy2-C6ChkfjUVIJHXFCqTVF9vjo?usp
 gdown --folder $NAUS_data_link
-unzip gigaword_10.zip
-unzip gigaword_13.zip
-unzip gigaword_8.zip
-unzip gigaword_ref.zip
-rm -rf gigaword_10.zip
-rm -rf gigaword_8.zip
-rm -rf gigaword_13.zip
-rm -rf gigaword_ref.zip
-
-mkdir model_weights
-mv *.pt model_weights
+unzip training_data/gigaword_10.zip
+unzip training_data/gigaword_13.zip
+unzip training_data/gigaword_8.zip
+unzip training_data/gigaword_ref.zip
+rm -rf training_data/gigaword_10.zip
+rm -rf training_data/gigaword_8.zip
+rm -rf training_data/gigaword_13.zip
+rm -rf training_data/gigaword_ref.zip
